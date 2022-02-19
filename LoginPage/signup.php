2064425b50bad8e4d839ddb03074bde2105fb10d
Muffin Cat <matanshemaya@gmail.com>
2022-02-19 23:48:16 +0200

    code

---
+++ LoginPage/signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sign Up Page</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <form action="signupacc.php" method="post">            
+            <h2>Sign Up</h2>
+            <Label>Name</Label>
+            <input type="text" name="name" placeholder="Name"><br>
+
+            <Label>User Name</Label>
+            <input type="text" name="uname" placeholder="User Name"><br>
+
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Password"><br>
+
+            <button type="submit">Sign Up</button>
+        </form>
+    </body>
+</html>
